bb8d70565b45b33e3ea3dc72918363520265b367
Cmepo <c.menendezperdomo@gmail.com>
2024-05-05 17:49:22 +0200

    Initial Commit

---
+++ main.py
@@ -0,0 +1,18 @@
+# Description: Main file to run the project
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Modules
+import DataValidation as dv
+import Forecasting as fc
+
+# Load the data
+df1 = pd.read_csv('Dataset For Forecasting Assignment.csv')
+
+dv.DataValidation(df1)
+fc.Forecast(df1)
+
+
+
